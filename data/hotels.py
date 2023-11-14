ed3ff092f8000134b396a0f5dc24f1e868c79fe9
flowerfrog <alexosenn@gmail.com>
2023-11-14 16:45:47 +0300

    Added dataclass SearchingHotel

--- data/hotels.py
+++ data/hotels.py
@@ -5,6 +5,12 @@ import dataclasses
 class Hotel:
     name: str
     address: str
+
+
+@dataclasses.dataclass
+class SearchingHotel:
+    name: str
+    address: str
     type: str
     services_in_the_hotel: list
     services_in_the_room: list
