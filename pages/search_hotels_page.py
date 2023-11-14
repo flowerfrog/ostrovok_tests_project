0c5449e5c4b689afeacb4306f418780094feacb2
flowerfrog <alexosenn@gmail.com>
2023-11-14 16:30:52 +0300

    Added search hotels page

---
+++ pages/search_hotels_page.py
@@ -0,0 +1,27 @@
+from selene import browser, have, be, query
+from selene.support.shared.jquery_style import ss
+
+
+class SearchHotelPage:
+    def open_search_page(self):
+        browser.open("hotel/russia/rostov-on-don/")
+        return self
+
+    def changing_filters_and_researching_hotel(self, hotel):
+        browser.element('button[data-testid="search-button"]').should(be.visible).click()
+        browser.element('input[name="price_to"]').should(be.visible).type(hotel.price_for_night)
+        browser.element('#apart').should(be.visible).execute_script('element.click()')
+        browser.element('#has_internet').execute_script('element.scrollIntoView()')
+        browser.element('#has_internet').should(be.visible).execute_script('element.click()')
+        browser.element('#has_parking').should(be.visible).execute_script('element.click()')
+        browser.element('#air-conditioning').should(be.visible).execute_script('element.click()')
+        browser.element('#balcony').should(be.visible).execute_script('element.click()')
+        browser.element('.zen-hotels-leftbar-regioninfo').execute_script('element.scrollIntoView()')
+        return self
+
+    def hotel_must_found(self, hotel):
+        ss('.zen-hotelcard-address.link').element_by(have.text(hotel.address)).should(be.visible)
+        browser.all(".zen-serpresultfilters").should(have.texts(f'{hotel.type}\n{hotel.services_in_the_hotel[0]}\n{hotel.services_in_the_hotel[1]}\n{hotel.services_in_the_room[0]}\n{hotel.services_in_the_room[1]}'))
+
+
+
