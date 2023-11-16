951f08897bc139f7ead35ca2b30015095ae65a9c
flowerfrog <alexosenn@gmail.com>
2023-11-16 10:21:57 +0300

    Added user's feedback dataclass

--- data/users.py
+++ data/users.py
@@ -9,3 +9,15 @@ class User:
     destination_country: str
     count_room: int
     count_guests: int
+
+
+@dataclasses.dataclass
+class UserFeedback:
+    name_user: str
+    email_user: str
+    hotel_name: str
+    hotel_city: str
+    hotel_country: str
+    questions: str
+
+
